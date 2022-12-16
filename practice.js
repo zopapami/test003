446181f49bd312a92fe8275b292f6be7441f1490
Zoi Papamichail <109663228+zopapami@users.noreply.github.com>
2022-12-16 16:11:36 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -16,9 +16,7 @@ async function getCats() {
         const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=cat', {mode: 'cors'});
         const catData = await response.json();
         myImg.src = catData.data.images.original.url;
-    } catch (e) {
-            console.log(e);
-    };
+    } catch (e) {console.log(e);};
 };
 getCats();
 */
